ddfc92bed650f8b5c8aa4c11c3e20349fab906fa
kobihanoch <kobikobi622@gmail.com>
2025-03-31 20:24:56 +0300

    style(statistics): updated UI

--- components/StatisticsComponents/ExercisesFlatList.js
+++ components/StatisticsComponents/ExercisesFlatList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
+import { Dimensions, FlatList, Text, View } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import ExerciseCard from "./ExerciseCard";
 
@@ -35,19 +35,5 @@ const ExercisesFlatList = ({ data, dataToCompare }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  item: {
-    marginRight: width * 0.05,
-    backgroundColor: "#f5f5f5",
-    width: width * 0.75,
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    paddingHorizontal: width * 0.05,
-    paddingVertical: height * 0.03,
-    gap: height * 0.02,
-    borderRadius: height * 0.02,
-  },
-});
 
 export default ExercisesFlatList;
