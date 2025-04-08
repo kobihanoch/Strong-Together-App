ea428070a035fe10365a4ba4d18d5d293dff8041
kobihanoch <kobikobi622@gmail.com>
2025-04-08 15:32:07 +0300

    chore(utils): created authUtils with function that checks if user worked out today

---
+++ utils/authUtils.js
@@ -0,0 +1,19 @@
+import { filterExercisesByDate } from "./statisticsUtils";
+import moment from "moment";
+
+export const hasWorkoutForToday = (exerciseTracking) => {
+  // Check if user trained today
+  if (exerciseTracking && exerciseTracking.length > 0) {
+    //console.log("Has et? " + (exerciseTracking != null));
+    const etByDate = filterExercisesByDate(
+      exerciseTracking,
+      moment().format("YYYY-MM-DD")
+    );
+    //console.log("HERE1");
+    if (etByDate && etByDate?.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
