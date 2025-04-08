6c5177d12055315dfc9e5dfa174614535481c074
kobihanoch <kobikobi622@gmail.com>
2025-04-08 18:12:20 +0300

    feat(profile): recreated profile page

---
+++ hooks/logic/useProfilePageLogic.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { useAuth } from "../../context/AuthContext";
+import { useUserWorkout } from "../useUserWorkout";
+import { getDaysSince } from "../../utils/homePageUtils";
+
+const useProfilePageLogic = (user) => {
+  const { updateProfilePic } = useAuth();
+  const { exerciseTracking, fetchUserExerciseTracking } = useUserWorkout(
+    user?.id
+  );
+  const [mediaLoading, setMediaLoading] = useState(false);
+  const [username, setUsername] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [fullname, setFullname] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [workoutCount, setWorkoutCount] = useState(0);
+  const [daysOnline, setDaysOnline] = useState(0);
+
+  // Initial load
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        setUsername(user.username);
+        setEmail(user.email);
+        setFullname(user.name);
+        const dataOfCreation = user.created_at.split("T")[0];
+        setDaysOnline(getDaysSince(dataOfCreation));
+        await fetchUserExerciseTracking();
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    })();
+  }, [user]);
+  useEffect(() => {
+    const uniWorkouts = new Set();
+    if (exerciseTracking && exerciseTracking.length > 0) {
+      exerciseTracking.forEach((exerciseInTrackingData) => {
+        uniWorkouts.add(exerciseInTrackingData.workoutdate);
+      });
+      setWorkoutCount(uniWorkouts.size);
+      setLoading(false);
+    }
+  }, [exerciseTracking]);
+
+  return {
+    data: {
+      username,
+      email,
+      fullname,
+      workoutCount,
+      daysOnline,
+    },
+    updateProfilePic,
+    loading,
+    mediaLoading,
+    setMediaLoading,
+  };
+};
+
+export default useProfilePageLogic;
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Dimensions, Text, View } from "react-native";
+import { Alert, Dimensions, Text, TouchableOpacity, View } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import LoadingPage from "../components/LoadingPage";
 import ImagePickerComponent from "../components/ProfileComponents/ImagePickerComponent";
 import { useAuth } from "../context/AuthContext";
+import useProfilePageLogic from "../hooks/logic/useProfilePageLogic";
 
 const { width, height } = Dimensions.get("window");
 
 function Profile({ navigation }) {
-  const { user, updateProfilePic } = useAuth();
-  const [mediaLoading, setMediaLoading] = useState(false);
-  const [username, setUsername] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [fullname, setFullname] = useState(null);
-
-  useEffect(() => {
-    setUsername(user.username);
-    setEmail(user.email);
-    setFullname(user.name);
-  }, [user]);
+  const { user } = useAuth();
+  const { data, loading, mediaLoading, updateProfilePic, setMediaLoading } =
+    useProfilePageLogic(user);
 
   return mediaLoading ? (
     <LoadingPage message="Loading user data..." />
@@ -48,30 +41,140 @@ function Profile({ navigation }) {
       </View>
       <View
         style={{
-          flex: 2,
+          flex: 3,
           width: "90%",
           justifyContent: "flex-start",
           alignItems: "center",
+          gap: height * 0.02,
+          flexDirection: "column",
         }}
       >
-        <Text style={{ fontFamily: "Inter_700Bold", fontSize: RFValue(17) }}>
-          {username}
-        </Text>
-        <Text
+        <View
           style={{
-            fontFamily: "Inter_400Regular",
-            fontSize: RFValue(15),
-            opacity: 0.3,
+            marginTop: height * 0.02,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          {fullname}
-        </Text>
-        <Text style={{ fontFamily: "Inter_700Bold", fontSize: RFValue(17) }}>
-          {email}
-        </Text>
+          <Text
+            style={{
+              fontFamily: "Inter_600SemiBold",
+              fontSize: RFValue(25),
+              color: "black",
+            }}
+          >
+            {data.fullname}
+          </Text>
+          <Text
+            style={{
+              fontFamily: "Inter_400Regular",
+              fontSize: RFValue(15),
+              color: "rgb(128, 128, 128)",
+            }}
+          >
+            {data.email}
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#2979FF",
+            borderRadius: height * 0.02,
+            width: "60%",
+            height: height * 0.05,
+            gap: width * 0.03,
+          }}
+          onPress={() => {
+            Alert.alert("Coming soon!");
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: RFValue(12),
+              fontFamily: "Inter_600SemiBold",
+            }}
+          >
+            Edit profile
+          </Text>
+        </TouchableOpacity>
       </View>
-      <View style={{ flex: 5 }}>
-        <Text>View</Text>
+      <View
+        style={{
+          flex: 4,
+          alignSelf: "center",
+          width: "80%",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: "white",
+            width: "100%",
+            height: "40%",
+            borderRadius: height * 0.04,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 0.05,
+            shadowRadius: 5,
+            elevation: 1,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              flex: 5,
+              gap: height * 0.01,
+            }}
+          >
+            <Text
+              style={{ fontFamily: "Inter_600SemiBold", fontSize: RFValue(14) }}
+            >
+              Workouts
+            </Text>
+            <Text
+              style={{
+                fontFamily: "Inter_400Regular",
+                fontSize: RFValue(20),
+                color: "rgb(128, 128, 128)",
+              }}
+            >
+              {data.workoutCount}
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              flex: 5,
+              borderLeftWidth: 1,
+              borderLeftColor: "rgba(233, 233, 233, 0.54)",
+              gap: height * 0.01,
+            }}
+          >
+            <Text
+              style={{ fontFamily: "Inter_600SemiBold", fontSize: RFValue(14) }}
+            >
+              Days Online
+            </Text>
+            <Text
+              style={{
+                fontFamily: "Inter_400Regular",
+                fontSize: RFValue(20),
+                color: "rgb(128, 128, 128)",
+              }}
+            >
+              {data.daysOnline.toString().split(" ")[0]}
+            </Text>
+          </View>
+        </View>
       </View>
     </View>
   );
