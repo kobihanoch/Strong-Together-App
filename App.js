0206f3ea29e3a9ab9d047fb253eabe1f910dbceb
kobihanoch <kobikobi622@gmail.com>
2025-05-17 12:30:32 +0300

    Fix works

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
-import 'react-native-url-polyfill/auto';
+import "react-native-url-polyfill/auto";
+import "react-native-get-random-values";
+import { decode, encode } from "base-64";
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 import {
   Inter_400Regular,
--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,8 @@
 // metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
-
 const config = getDefaultConfig(__dirname);
 
-config.resolver.extraNodeModules = {
-  // כל מודולי הליבה שתומכים ב-browserify
-  ...require("node-libs-react-native"),
-  // map ws ו־ws/lib/websocket אל ה־shim שלכם
-  ws: path.resolve(__dirname, "shims/ws.js"),
-  "ws/lib/websocket": path.resolve(__dirname, "shims/ws.js"),
-  // stream – רק אם באמת יש require('stream') אי-שם
-  stream: require.resolve("stream-browserify"),
-  // אם נתקלתם ב־http/https/tls כבר לא אמורים ליפול
-};
-
-config.resolver.sourceExts.push("cjs");
+// תעדיף קודם react-native ואז browser ואז main (ולא לבטל exports)
+config.resolver.resolverMainFields = ["react-native", "browser", "main"];
 
 module.exports = config;
