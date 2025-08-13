1d65ffc9b89b3a6f70beb90cc1af0b7e9608b281
Kobi Hanoch <kobikobi622@gmail.com>
2025-08-13 15:25:46 +0300

    feat(FinishWorkout): Workout save new endpoint impletantion and save to cache

--- hooks/useUserWorkout.js
+++ hooks/useUserWorkout.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { saveWorkoutData } from "../services/WorkoutService";
 
 export const useUserWorkout = (userId) => {
-  const [userWorkout, setUserWorkout] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [saving, setSaving] = useState(false);
@@ -10,7 +9,8 @@ export const useUserWorkout = (userId) => {
   const saveWorkoutProccess = async (workoutData) => {
     setSaving(true);
     try {
-      await saveWorkoutData(workoutData);
+      const data = await saveWorkoutData(workoutData);
+      return data;
     } catch (err) {
       console.error(err);
       throw err;
