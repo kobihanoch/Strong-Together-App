6941c5f71ae20b135de122ab54b135d4ad964585
Kobi Hanoch <kobikobi622@gmail.com>
2025-08-15 16:46:40 +0300

    feat(Utils): New util to extract workout splits from workout

--- utils/sharedUtils.js
+++ utils/sharedUtils.js
@@ -36,3 +36,13 @@ export const splitTheWorkout = (userWorkout) => {
     return {};
   }
 };
+
+// Returns an obj
+// {workoutSplits = [A,B,C,...], exercises = {A: [exercises...], B: [exercises...]}}
+export const extractWorkoutSplits = (workout) => {
+  const map = workout.workoutsplits.reduce((acc, split) => {
+    acc[split.name] = [...split.exercisetoworkoutsplit];
+    return acc;
+  }, {});
+  return { workoutSplits: Object.keys(map), exercises: map };
+};
