ff2b76cd72a31c4cfb5ae3c0c47b17a06edc2de9
Kobi Hanoch <kobikobi622@gmail.com>
2026-03-22 22:18:02 +0200

    Refactored error alerts  to TS

--- errors/errorAlerts.ts
+++ errors/errorAlerts.ts
@@ -1,12 +1,12 @@
-import { Notifier, NotifierComponents } from "react-native-notifier";
+import { Notifier, NotifierComponents } from 'react-native-notifier';
 
-export function showErrorAlert(title, description) {
+export function showErrorAlert(title: string, description: string): void {
   Notifier.showNotification({
     title,
     description,
     Component: NotifierComponents.Alert,
     componentProps: {
-      alertType: "error",
+      alertType: 'error',
     },
     duration: 4000,
     showAnimationDuration: 300,
