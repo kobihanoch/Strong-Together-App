1351f7c85cb72c66678c4b6372e2f1a62312c04a
Kobi Hanoch <kobikobi622@gmail.com>
2026-04-25 11:55:50 +0300

    Updated app config version

--- app.config.ts
+++ app.config.ts
@@ -23,7 +23,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
     name: isProd ? APP_NAME_BASE : `${APP_NAME_BASE} (Dev)`,
     scheme: 'strongtogether',
     slug: 'strong-together',
-    version: '5.2.0',
+    version: '5.2.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
