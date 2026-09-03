7ebf803cd55fdba04c8d0a178f876dd960224bb8
Kobi Hanoch <kobikobi622@gmail.com>
2026-09-03 21:21:15 +0300

    Removed unused components

--- features/workouts/analytics/services/analytics.service.ts
+++
@@ -1,7 +0,0 @@
-import api from '../../../../infrastructure/api/api-config/api';
-import { GetAnalyticsResponse } from '@strong-together/shared';
-
-export const getTrackingAnalytics = async (): Promise<GetAnalyticsResponse> => {
-  const { data } = await api.get<GetAnalyticsResponse>('/api/analytics');
-  return data;
-};
